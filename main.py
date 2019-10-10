2b285d81666dffea83a0390edbd41a6a456909cd
Brendan Davy <davybrendan@gmail.com>
2019-10-09 22:53:07 -0700

    Update main.py
    outline

---
+++ main.py
@@ -0,0 +1,22 @@
+##rules
+##two players
+##board is to be printed out after each move
+##accept input of player position and place symbol on board
+
+##set play_game = True
+
+##while(play_game)
+
+    ##ask player 1 if X or O
+    ##player 1 will go first.
+
+    ##ready?
+    ##print board
+    ##while(game not over)
+        ##receive positional input
+        ##add to board
+        ##check if winner
+        ##printboard
+
+    ##player X has won
+    ##play again?
