d2e15b59dac7036f80b8ca33449b41b028d08540
Brendan Davy <davybrendan@gmail.com>
2019-10-10 17:48:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
-##rules
-##two players
-##board is to be printed out after each move
-##accept input of player position and place symbol on board
+# rules
+# two players
+# board is to be printed out after each move
+# accept input of player position and place symbol on board
 
-##set play_game = True
 
-##while(play_game)
+def main():
+    play_game = True
 
-    ##ask player 1 if X or O
-    ##player 1 will go first.
+    while play_game:
+        determine_marks()
 
-    ##ready?
-    ##print board
-    ##while(game not over)
-        ##receive positional input
-        ##add to board
-        ##check if winner
-        ##printboard
+    # player 1 will go first.
 
-    ##player X has won
-    ##play again?
+    # ready?
+    # print board
+    # while(game not over)
+    # receive positional input
+    # add to board
+    # check if winner
+    # print board
+
+    # player X has won
+    # play again?
+
+
+def determine_marks():
+    # ask player 1 if X or O
+    valid_answer = False
+    while not valid_answer:
+        p1_s = input("Player 1: Would you like X or O? ")
