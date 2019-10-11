d561c37ee2b6af06828602d08df405d91fc1b0bb
Brendan Davy <davybrendan@gmail.com>
2019-10-10 21:31:41 -0700

    Update main.py
    completed project.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # rules
 # two players
+# ask Player 1 if X or O
+# ask if Ready
 # board is to be printed out after each move
 # accept input of player position and place symbol on board
+# after game is over, ask to play again
 
 
 def main():
@@ -25,14 +28,22 @@ def main():
         # print board
         game_over = False
         board_state = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-        current_player = "Player 1"
+        current_player = "Player 2"
         while not game_over:
+            current_player = next_player(current_player)
             draw_board(board_state)
             board_state = receive_play(current_player, marks, board_state)
-
-
-    # player X has won
-    # play again?
+            game_over = check_winner(board_state)
+            # if we filter board_state to []
+            if not filter_occupied(board_state):
+                print("The game is a draw.")
+                break
+        # only if the game has actually been won.
+        draw_board(board_state)
+        if game_over:
+            print("{} has won the game!".format(current_player))
+        # play again?
+        play_game = play_again()
 
 
 def determine_marks():
@@ -59,6 +70,13 @@ def ready():
         valid_answer = False
 
 
+def next_player(current_player):
+    if current_player == "Player 1":
+        return "Player 2"
+    else:
+        return "Player 1"
+
+
 def draw_board(board_state):
     print(" {} | {} | {} ".format(board_state[6], board_state[7], board_state[8]))
     print("-----------")
@@ -72,14 +90,48 @@ def receive_play(current_player, marks, board_state):
     while not valid_answer:
         current_play = input(
             "{}, choose a square (1-9) to place your {}. ".format(current_player, marks[current_player]))
-        valid_answer = check_inputs(["1", "2", "3", "4", "5", "6", "7", "8", "9"], current_play)
-    current_play = int(current_play)
-    if board_state[current_play - 1] != " ":
-        print("Position {} is already occupied. Choose again.".format(current_play))
-        return receive_play(current_player, marks, board_state)
+        valid_answer = check_inputs(filter_occupied(board_state), current_play)
+        if valid_answer:
+            current_play = int(current_play)
+            board_state[current_play - 1] = marks[current_player]
+    return board_state
+
+
+def filter_occupied(board_state):
+    my_list = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    for i in range(0, 9):
+        if board_state[i] != " ":
+            my_list.remove(str(i+1))
+    return my_list
+
+
+def check_winner(board_state):
+    if board_state[0] == board_state[1] == board_state[2] and board_state[0] != " ":
+        return True
+    elif board_state[0] == board_state[4] == board_state[8] and board_state[0] != " ":
+        return True
+    elif board_state[0] == board_state[3] == board_state[6] and board_state[0] != " ":
+        return True
+    elif board_state[3] == board_state[4] == board_state[5] and board_state[3] != " ":
+        return True
+    elif board_state[6] == board_state[7] == board_state[8] and board_state[6] != " ":
+        return True
+    elif board_state[6] == board_state[4] == board_state[2] and board_state[6] != " ":
+        return True
+    elif board_state[7] == board_state[4] == board_state[1] and board_state[7] != " ":
+        return True
+    elif board_state[8] == board_state[5] == board_state[2] and board_state[8] != " ":
+        return True
     else:
-        board_state[current_play - 1] = marks[current_player]
-        return board_state
+        return False
+
+
+def play_again():
+    valid_answer = False
+    while not valid_answer:
+        response = input("Would you like to play again? ")
+        valid_answer = check_inputs(["Y", "N"], response[0].capitalize())
+    return response[0].capitalize() == "Y"
 
 
 def check_inputs(acceptable, my_s):
